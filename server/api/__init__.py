1aa05b31f8a6fe8185898f0ee47210b5ec0dd285
Lars Fischer <lars.fischer@rwth-aachen.de>
2019-10-18 11:45:54 +0200

    optimicer structure

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -7,6 +7,7 @@ from .achievement_assignment import api as ns4
 from .experience_assignment import api as ns5
 from .vehicle import api as ns6
 from .environment import api as ns7
+from .booking_optimizer import api as ns8
 
 api = Api(
     title='eGOn API',
@@ -21,4 +22,5 @@ api.add_namespace(ns4, path='/api/achievement_assignment')
 api.add_namespace(ns5, path='/api/experience_assignment')
 api.add_namespace(ns6, path='/api/vehicle')
 api.add_namespace(ns7, path='/api/environment')
+api.add_namespace(ns8, path='/api/booking/optimizer')
 
