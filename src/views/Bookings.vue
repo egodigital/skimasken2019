7459826546bff93b5af6ea572cc3df37a829bbbe
Philipp Schulz <schulz.philipp@outlook.de>
2019-10-18 03:42:40 +0200

    Added datetimepicker and bookings form

--- src/views/Bookings.vue
+++ src/views/Bookings.vue
@@ -1,10 +1,60 @@
 <template>
-  <h1>Bookings</h1>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="3">
+        <datetime-picker :timePickerProps="{'allowed-minutes': [0, 15, 30, 45]}" label="From" v-model="date.from"></datetime-picker>
+      </v-col>
+      <v-col cols="12" md="3">
+        <datetime-picker :timePickerProps="{'allowed-minutes': [0, 15, 30, 45]}" label="To" v-model="date.to"></datetime-picker>
+      </v-col>
+      <v-col cols="12" md="2">
+        <v-select
+          :items="vehicles"
+          label="Standard"
+        ></v-select>
+      </v-col>
+      <v-col cols="12" md="1">
+        <v-checkbox
+          v-model="isFuzzy"
+          label="Fuzzy?"
+        ></v-checkbox>
+      </v-col>
+      <v-col cols="12" md="3" v-if="isFuzzy">
+        <v-text-field
+          label="Duration in minutes"
+          type="number"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
+import DatetimePicker from '@/components/DatetimePicker'
 
+export default {
+  components: {
+    DatetimePicker
+  },
+  data() {
+    return {
+      date: {
+        from: '',
+        to: ''
+      },
+      fuzzyDate: {
+        from: '',
+        to: ''
+      },
+      isFuzzy: false,
+      vehicles: [
+        {
+          text: 'Vehicle 1',
+          value: 'dsds'
+        }
+      ]
+    }
+  }
 }
 </script>
 
